7a696b1e653d2b6406229eb666f440e6d372d7c5
Julia Veronica <julia.veronicagdu@gmail.com>
2020-04-26 16:11:22 +0700

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+const urlAPI= "http://jasonplaceholder.typicode.com/todos/1"
+axios.get()
+
+// fetch(`${urlAPI}/post/1`,{
+//     method:"DELETE"
+// })
+// // .then(res.json())
+// // {
+//     //cara 1.
+//     // console,log(res);
+//     // return res.json();
+// // }
+// .then((res)=>{
+//     //cara 2.
+//     if(res.ok){
+//         return res.json();
+//     }
+//     else{
+//         throw new Error("GAGAL")
+//     }
+// })
+// .then((json => console.log(json))
+// .catch(err => console.log(err))
+
